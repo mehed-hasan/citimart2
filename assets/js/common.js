79256627cb890dc8b19dae96e2e7824af518c13b
mehed-hasan <mehedi.hasan@intelli.global>
2022-06-12 18:50:38 +0600

    Carees page

--- assets/js/common.js
+++ assets/js/common.js
@@ -26,3 +26,71 @@
 $("#categoryMenuToggle").on("click", function () {
   $(".all_department_menu").slideToggle(500);
 });
+
+
+
+
+
+
+
+
+// Js for modal started 
+
+$(window).on('load', function() {
+  $(".location_modal").modal('show');
+
+  $(".sign_in_btn").click(function(){
+    $(".modal").modal('hide');
+    $(".sign_in_modal").modal('show');
+  });
+
+  $(".sign_up_btn").click(function(){
+    $(".modal").modal('hide');
+    $(".sign_up_modal_1").modal('show');
+  });
+
+
+  $(".modal_back").click(function(){
+    var prev = $(this).attr("data-prev");
+          $(this).parent().parent().parent().parent().parent().modal('hide');
+          $("."+prev).modal('show');
+  })
+});
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+
+  (function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation();
+          }
+
+          else{
+            // If validated 
+
+          // Remove those line when it comes from back end 
+          event.preventDefault()
+          event.stopPropagation();
+          var next = $(this).find("button:last-child").attr("data-next");
+          $(this).parent().parent().parent().modal('hide');
+          $("."+next).modal('show');
+           
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+  // Modal js ended 
